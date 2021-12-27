6232264cfa6fcc2ea14872343af63e8ddaba3c10
Eui Hyun Kim <hyomithetech@gmail.com>
2021-12-26 22:10:28 -0800

    Complete lesson 7 and 8, and styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react"
+import {Link} from 'gatsby'
 import Layout from '../component/Layout'
+import { header, btn} from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p> US designer & web developer based in Vegas.</p>
+          <Link className={btn} to="/projects">My Portfolio Projects</Link>
         </div>
+        <img src="/banner.png" alt="site banner" style={{maxWidth: "100%"}}/>
       </section>
     </Layout>
   )
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../component/Layout'
+import {portfolio} from '../../styles/projects.module.css'
 
 export default function Projects() {
     return (
         <Layout>
-            <div>
+            <div className={portfolio}>
                 <h2>Portfolio</h2>
                 <h3>Projects & Websites I've Created</h3>
             </div>
